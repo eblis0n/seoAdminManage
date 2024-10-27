03249de77e6bab0fd58d945f6dc06ac3377a2d9e
eblis0n <eblis0n@hotmail.com>
2024-10-28 02:31:38 +0800

    修复bug

--- src/api/amazonsqsAPI/index.ts
+++ src/api/amazonsqsAPI/index.ts
@@ -2,7 +2,7 @@
  * @version: 1.0.0
  * @Author: Eblis
  * @Date: 2023-10-17 21:21:36
- * @LastEditTime: 2024-10-27 21:37:45
+ * @LastEditTime: 2024-10-27 23:04:43
  */
 //统一管理域名相关的接口
 import request from "@/utils/axios/request";
@@ -10,7 +10,7 @@ import request from "@/utils/axios/request";
 import type { amazonsqsResult } from "@/types/other";
 
 enum API {
-  AMAZONSQS_LIST = "/sys/user/login/",
+  AMAZONSQS_LIST = "/ama/list/",
 }
 class amazonsqsAPI {
   static requestamazonSQSList = () =>
